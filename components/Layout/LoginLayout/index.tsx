ab6af59a11807505567ee09da3ca85cb94e12ab2
maxidefilippis <maxidefilippis@gmail.com>
2022-03-09 01:28:36 -0300

    feature: improved login layout

--- components/Layout/LoginLayout/index.tsx
+++ components/Layout/LoginLayout/index.tsx
@@ -1,5 +1,15 @@
 import styles from "./index.module.css";
 
 export const LoginLayout: React.FC = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <a href="/">BAST! </a>
+        <p>Sistema inmobiliario</p>
+      </header>
+      <div className={styles.cover} />
+      <main className={styles.main}>{children}</main>
+      <footer className={styles.footer}>© 2022 Drimax Group</footer>
+    </div>
+  );
 };
